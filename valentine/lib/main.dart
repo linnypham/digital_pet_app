a2a977ed7681028ebc6e90ef988395ee0f1afcd3
linnypham <89488749+linnypham@users.noreply.github.com>
2025-02-19 12:25:43 -0500

    confetii

--- valentine/lib/main.dart
+++ valentine/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
+import 'dart:async';
 
 void main() => runApp(const MyApp());
 
@@ -26,13 +28,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController motionController;
   late Animation<double> motionAnimation;
+  late ConfettiController _leftConfettiController;
+  late ConfettiController _rightConfettiController;
+  late Timer _timer;
+  bool _shootLeft = true; 
 
   @override
   void initState() {
     super.initState();
 
+    
     motionController = AnimationController(
-      duration: const Duration(seconds: 1), //duration of the animation
+      duration: const Duration(seconds: 1),
       vsync: this,
       lowerBound: 0.5,
     );
@@ -50,11 +57,28 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         motionController.forward();
       }
     });
+
+    
+    _leftConfettiController = ConfettiController(duration: const Duration(seconds: 1));
+    _rightConfettiController = ConfettiController(duration: const Duration(seconds: 1));
+
+    
+    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (_shootLeft) {
+        _leftConfettiController.play();
+      } else {
+        _rightConfettiController.play();
+      }
+      _shootLeft = !_shootLeft; 
+    });
   }
 
   @override
   void dispose() {
     motionController.dispose();
+    _leftConfettiController.dispose();
+    _rightConfettiController.dispose();
+    _timer.cancel();
     super.dispose();
   }
 
@@ -65,39 +89,73 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 200, left: 8, right: 8),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your message...',
-              ),
-            ),
-            Center(
-              child: SizedBox(
-                height: 200,
-                child: Stack(
-                  children: <Widget>[
-                    Center(
-                      child: AnimatedBuilder(
-                        animation: motionController,
-                        builder: (context, child) {
-                          return SizedBox(
-                            height: motionController.value * 250,
-                            child: child,
-                          );
-                        },
-                        child: Image.asset('assets/images/heart.png'),
-                      ),
-                    ),
-                  ],
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 200, left: 8, right: 8),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your message...',
+                  ),
                 ),
-              ),
+                Center(
+                  child: SizedBox(
+                    height: 200,
+                    child: Stack(
+                      children: <Widget>[
+                        Center(
+                          child: AnimatedBuilder(
+                            animation: motionController,
+                            builder: (context, child) {
+                              return SizedBox(
+                                height: motionController.value * 250,
+                                child: child,
+                              );
+                            },
+                            child: Image.asset('assets/images/heart.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+
+          // Left
+          Align(
+            alignment: Alignment.centerLeft,
+            child: ConfettiWidget(
+              confettiController: _leftConfettiController,
+              blastDirection:
+                  -3.14 / 4, 
+              emissionFrequency: 0.05,
+              numberOfParticles: 20,
+              maxBlastForce: 100,
+              minBlastForce: 80,
+              gravity: 0.3,
+            ),
+          ),
+
+          // Right
+          Align(
+            alignment: Alignment.centerRight,
+            child: ConfettiWidget(
+              confettiController: _rightConfettiController,
+              blastDirection:
+                  -3 * (3.14 / 4), 
+              emissionFrequency: 0.05,
+              numberOfParticles: 20,
+              maxBlastForce: 100,
+              minBlastForce: 80,
+              gravity: 0.3,
+            ),
+          ),
+        ],
       ),
     );
   }
