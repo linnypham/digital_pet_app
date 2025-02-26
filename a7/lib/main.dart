fb6c57a92a34e58c76b8bc644b0b0d4753943e1c
ClearNixson <nixsonyeung13612@gmail.com>
2025-02-26 14:22:04 -0500

    Color picker final

--- a7/lib/main.dart
+++ a7/lib/main.dart
@@ -20,6 +20,7 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color _textColor = Colors.black;
 
   void toggleVisibility() {
     setState(() {
@@ -27,11 +28,59 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     });
   }
 
+  void _showColorPicker() async {
+    Color pickerColor = _textColor;
+    Color currentColor = _textColor;
+
+    final Color? selectedColor = await showDialog<Color>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color!'),
+          content: SingleChildScrollView(
+            child: Container(
+              width: double.maxFinite, // Ensure the container takes full width
+              child: ColorPicker(
+                pickerColor: pickerColor,
+                onColorChanged: (color) {
+                  pickerColor = color;
+                },
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Got it'),
+              onPressed: () {
+                setState(() {
+                  currentColor = pickerColor;
+                });
+                Navigator.of(context).pop(currentColor);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (selectedColor != null) {
+      setState(() {
+        _textColor = selectedColor;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.color_lens),
+            onPressed: _showColorPicker,
+          ),
+        ],
       ),
       body: Center(
         child: AnimatedOpacity(
@@ -39,7 +88,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
           duration: Duration(seconds: 1),
           child: Text(
             'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+            style: TextStyle(fontSize: 24, color: _textColor),
           ),
         ),
       ),
@@ -47,6 +96,63 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
         onPressed: toggleVisibility,
         child: Icon(Icons.play_arrow),
       ),
+    );
+  }
+}
+
+class ColorPicker extends StatefulWidget {
+  final Color pickerColor;
+  final ValueChanged<Color> onColorChanged;
+
+  ColorPicker({
+    required this.pickerColor,
+    required this.onColorChanged,
+  });
+
+  @override
+  _ColorPickerState createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  late Color _currentColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentColor = widget.pickerColor;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: GridView.count(
+            crossAxisCount: 4,
+            children: List.generate(Colors.primaries.length, (index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _currentColor = Colors.primaries[index];
+                  });
+                  widget.onColorChanged(_currentColor);
+                },
+                child: Container(
+                  color: Colors.primaries[index],
+                  margin: EdgeInsets.all(8.0),
+                ),
+              );
+            }),
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 50,
+          color: _currentColor,
+          margin: EdgeInsets.all(16.0),
+        ),
+      ],
     );
   }
 }
